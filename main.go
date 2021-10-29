326b1ef58bc19faa75eb054ab5f569d33dc25a2e
Debanjan Dasgupta <Dasgupta_Debanjan@bah.com>
2021-10-28 21:17:10 -0400

    update

--- main.go
+++ main.go
@@ -5,13 +5,19 @@ import (
 )
 
 func main() {
-	InitializeCrispInputs()
-	InitializeFuzzyInputs()
+	f := FuzzyAssetAllocator{}
+	f.Initialize()
+	f.InitializeFuzzyInputs()
+	f.ProcessRules()
+	f.CalculateResultantAssetAllocation()
+	f.FactorizeReasons()
 }
 
 
 const(
 	MeanCAPERatio = 16.85
+	stocks = "stocks"
+	bonds = "bonds"
 )
 
 type VariableNameEnum struct {
@@ -65,20 +71,31 @@ type Input struct {
 	Value float64
 }
 
-var CrispInputs = map[string]Input{}
-var FuzzyInputs = map[string]Input{}
+type Weight struct {
+	Asset string
+	Weight float64
+	Reason string
+	Contribution float64
+}
+
+type FuzzyAssetAllocator struct {
+	CrispInputs map[string]Input
+	FuzzyInputs map[string]Input
+	Weights []Weight
+	TotalWeight float64
+}
 
 
-func AddCrispInput(name string, value float64) {
-	CrispInputs[name] = Input{
+func (f *FuzzyAssetAllocator) AddCrispInput(name string, value float64) {
+	f.CrispInputs[name] = Input{
 		Name:  name,
 		Value: value,
 	}
 	fmt.Printf("%v initialized to: %v\n",name, value)
 }
 
-func AddFuzzyVariable(name string, value float64) {
-	FuzzyInputs[name] = Input{
+func (f *FuzzyAssetAllocator) AddFuzzyVariable(name string, value float64) {
+	f.FuzzyInputs[name] = Input{
 		Name:  name,
 		Value: value,
 	}
@@ -86,13 +103,14 @@ func AddFuzzyVariable(name string, value float64) {
 
 }
 
-func InitializeCrispInputs() {
+func (f *FuzzyAssetAllocator) Initialize() {
+	f.CrispInputs = map[string]Input{}
 	fmt.Println("Crisp Inputs")
-	AddCrispInput(VariableNames.CAPERatio, 37.63)
-	AddCrispInput(VariableNames.FEDFundsRate, 0.25)
-	AddCrispInput(VariableNames.TimeHorizon, 20.0)
-	AddCrispInput(VariableNames.Cashflow, 1.0)
-	AddCrispInput(VariableNames.CPI, 4.8)
+	f.AddCrispInput(VariableNames.CAPERatio, 37.63)
+	f.AddCrispInput(VariableNames.FEDFundsRate, 0.25)
+	f.AddCrispInput(VariableNames.TimeHorizon, 20.0)
+	f.AddCrispInput(VariableNames.Cashflow, 1.0)
+	f.AddCrispInput(VariableNames.CPI, 4.8)
 }
 
 func normalize(val float64) float64 {
@@ -106,13 +124,11 @@ func normalize(val float64) float64 {
 
 
 
-func Line(varName string, min, max float64) float64 {
-	v := CrispInputs[varName]
+func Line(v Input, min, max float64) float64 {
 	return normalize((v.Value-min)/(max-min))
 }
 
-func Triangle(varName string, min, middle, max float64) float64 {
-	v := CrispInputs[varName]
+func Triangle(v Input, min, middle, max float64) float64 {
 	if v.Value < middle {
 		return normalize((v.Value - min) / (middle - min))
 	} else {
@@ -121,22 +137,40 @@ func Triangle(varName string, min, middle, max float64) float64 {
 }
 
 
-
-func InitializeFuzzyInputs() {
+// InitializeFuzzyInputs sets the membership functions for
+func (f *FuzzyAssetAllocator) InitializeFuzzyInputs() {
 	fmt.Println("\n\nFuzzy Inputs")
-	AddFuzzyVariable(VariableNames.HighGrowth, Line(VariableNames.CAPERatio, MeanCAPERatio, 12.0))
-	AddFuzzyVariable(VariableNames.LowGrowth, Line(VariableNames.CAPERatio, MeanCAPERatio, 30.0))
-	AddFuzzyVariable(VariableNames.LowRate, Line(VariableNames.FEDFundsRate, 2.0, -2.0))
-	AddFuzzyVariable(VariableNames.HighRate, Line(VariableNames.FEDFundsRate, 2.0, -6.0))
-	AddFuzzyVariable(VariableNames.ShortTerm, Line(VariableNames.TimeHorizon, 5.0, 1.0))
-	AddFuzzyVariable(VariableNames.LongTerm, Line(VariableNames.TimeHorizon, 5.0, 15.0))
-	AddFuzzyVariable(VariableNames.Accumulation, Line(VariableNames.Cashflow, 0, 20.0))
-	AddFuzzyVariable(VariableNames.Drawdown, Triangle(VariableNames.Cashflow, 0, -3.0, -10.0))
-	AddFuzzyVariable(VariableNames.Deflation, Line(VariableNames.TimeHorizon, 0.0, -3.0))
-	AddFuzzyVariable(VariableNames.LowInflation, Triangle(VariableNames.TimeHorizon, 0.0, 3.0, 7.0))
-	AddFuzzyVariable(VariableNames.HighInflation, Line(VariableNames.TimeHorizon, 5.0, 15.0))
+	f.AddFuzzyVariable(VariableNames.HighGrowth, Line(f.CrispInputs[VariableNames.CAPERatio], MeanCAPERatio, 12.0))
+	f.AddFuzzyVariable(VariableNames.LowGrowth, Line(f.CrispInputs[VariableNames.CAPERatio], MeanCAPERatio, 30.0))
+	f.AddFuzzyVariable(VariableNames.LowRate, Line(f.CrispInputs[VariableNames.FEDFundsRate], 2.0, -2.0))
+	f.AddFuzzyVariable(VariableNames.HighRate, Line(f.CrispInputs[VariableNames.FEDFundsRate], 2.0, 10.0))
+	f.AddFuzzyVariable(VariableNames.ShortTerm, Line(f.CrispInputs[VariableNames.TimeHorizon], 5.0, 1.0))
+	f.AddFuzzyVariable(VariableNames.LongTerm, Line(f.CrispInputs[VariableNames.TimeHorizon], 5.0, 15.0))
+	f.AddFuzzyVariable(VariableNames.Accumulation, Line(f.CrispInputs[VariableNames.Cashflow], 0, 20.0))
+	f.AddFuzzyVariable(VariableNames.Drawdown, Triangle(f.CrispInputs[VariableNames.Cashflow], 0, -3.0, -10.0))
+	f.AddFuzzyVariable(VariableNames.Deflation, Line(f.CrispInputs[VariableNames.TimeHorizon], 0.0, -3.0))
+	f.AddFuzzyVariable(VariableNames.LowInflation, Triangle(f.CrispInputs[VariableNames.TimeHorizon], 0.0, 3.0, 7.0))
+	f.AddFuzzyVariable(VariableNames.HighInflation, Line(f.CrispInputs[VariableNames.TimeHorizon], 5.0, 15.0))
 }
 
-func ProcessRules() {
+func (f *FuzzyAssetAllocator) ProcessRules() {
+
+}
+
+func (f *FuzzyAssetAllocator) CalculateResultantAssetAllocation() float64 {
+	s := 0.0
+	b := 0.0
+	for _, w := range f.Weights {
+		if w.Asset == stocks {
+			s += w.Weight
+		} else {
+			b += w.Weight
+		}
+	}
+	f.TotalWeight = s+b
+	return s/f.TotalWeight
+}
+
+func (f *FuzzyAssetAllocator) FactorizeReasons() {
 
 }
